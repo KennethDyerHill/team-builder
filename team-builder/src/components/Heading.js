834a643fffac706a55a92e247b69071f6f3653f7
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2019-09-19 10:45:00 -0400

    Update

---
+++ team-builder/src/components/Heading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Heading(){
+    return(
+        <h1 className='heading'>Team Builder</h1>
+    )
+}
+
+export default Heading;
---
+++ team-builder/src/data.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: 'Ken',
+        email: 'fakeEmail@youfellforit.com',
+        role: 'Full stack web deleter'
+    },
+    {
+        name: 'John',
+        email: 'MarstonRancher@redemption.com',
+        role: '"Rancher"'
+    }
+];
