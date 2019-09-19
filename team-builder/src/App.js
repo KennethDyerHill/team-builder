e59d59c41239278c79f005327dfdb02329f790bf
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2019-09-19 10:37:28 -0400

    Update

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import data from './data.js';
+import Heading from './components/heading/Heading.js';
+import Form from './components/form/Form.js';
+import MembersList from './components/membersList/MembersList.js';
 
 function App() {
+  const [list, setList] = useState(data);
+  
+  const newMember = member => {
+    setList([...list, member]);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <Form newMember={newMember} />
+      <MembersList membersList={list} />
+
     </div>
   );
 }
